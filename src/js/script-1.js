caaee587f1eb3f53b7a8fda84e85402695c89378
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-28 00:06:58 +0200

    Feat(js): added script to task 1 -task done

---
+++ src/js/script-1.js
@@ -0,0 +1,11 @@
+const name = 'Генератор захисного поля';
+
+let price = 1000;
+
+// document.write(`Обрано ${name}, ціна за штуку ${price} кредитів`);
+// console.log(`Обрано ${name}, ціна за штуку ${price} кредитів`);
+
+price = 2000;
+
+// document.write(`Обрано ${name}, ціна за штуку ${price} кредитів`);
+console.log(`Обрано ${name}, ціна за штуку ${price} кредитів`);
